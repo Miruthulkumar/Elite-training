04f37f4e9e223324c090421ed24b4f19b72c73a4
MiruthulKumar <saimiruthul@gmail.com>
2025-08-08 12:46:47 +0530

    camel case change

--- Day 1/Encapsu.java
+++ Day 1/Encapsu.java
@@ -1,24 +1,24 @@
 class Student{
     private String name;
     private int age;
-    public void setname(String name){
+    public void setName(String name){
         this.name=name;
     }
-    public void setage(int age){
+    public void setAge(int age){
         this.age=age;
     }
-    public String getname(){
+    public String getName(){
         return name;
     }
-    public int getage(){
+    public int getAge(){
         return age;
     }
 }
 public class Encapsu{
     public static void main(String[] args){
         Student s1 = new Student();
-        s1.setname("Miruthul");
-        s1.setage(19);
-        System.out.println(s1.getname()+ " " + s1.getage());
+        s1.setName("Miruthul");
+        s1.setAge(19);
+        System.out.println(s1.getName()+ " " + s1.getAge());
     }
 }
