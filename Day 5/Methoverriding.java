c77736289d7a9439ce0e12dd91bb2a8e67dd1690
MiruthulKumar <saimiruthul@gmail.com>
2025-08-08 10:58:04 +0530

    overriding

---
+++ Day 5/Methoverriding.java
@@ -0,0 +1,26 @@
+class Animal{
+    void Sound(){
+        System.out.println("Animal Makes Sounds!");
+    }
+}
+
+class Dog extends Animal{
+    void Sound(){
+        System.out.println("Dog is Barking!");
+    }
+}
+
+class Cat extends Animal{
+    void Sound(){
+        System.out.println("Cat is Meowing!");
+    }
+}
+
+public class Methoverriding {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        a1.Sound();
+        Animal a2 = new Cat();
+        a2.Sound();
+    }    
+}
